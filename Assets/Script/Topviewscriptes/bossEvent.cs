bce7db45a0d0bb81585a12b0f7e43ad62256dfe5
dsfsada <dsfsada@naver.com>
2024-05-16 19:16:10 +0900

    중간 업데이트
    boss에게 죽지 않는 문제 해결

--- Assets/Script/Topviewscriptes/bossEvent.cs
+++ Assets/Script/Topviewscriptes/bossEvent.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class bossEvent : MonoBehaviour
 {
+    int randAtk;
     public float maxHealth, minHealth, health, dropItem, atk, attackCool;
     private Player player;
+    private Rigidbody2D rb;
 
     public Animator animator;
 
@@ -15,48 +17,52 @@ public class bossEvent : MonoBehaviour
         health = (int)Random.Range(minHealth, maxHealth);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (health <= 0)
-        {
-            DeadEvent();
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Player") && player == null)
         {
             player = col.GetComponent<Player>();
-            if (player.health > 0)
-            {
-                StartCoroutine(AttackPlayer(col));
-            }
+            rb = player.GetComponent<Rigidbody2D>();
+            StartCoroutine(AttackPlayer(col));
+        }
+        else
+        {
+            StartCoroutine(AttackPlayer(col));
         }
     }
 
     private IEnumerator AttackPlayer(Collider2D col)
     {
-        while (player.health > 0) // 적의 체력이 0보다 클 때까지 반복
+        yield return new WaitForSeconds(attackCool);
+
+        while (player.health > 0 && health > 0) // 적의 체력이 0보다 클 때까지 반복
         {
-            yield return new WaitForSeconds(attackCool);
+            randAtk = Random.Range(0, 10);
 
             animator.SetBool("atk2", true);
-            //Attack(col);
 
-            break;
+            if(randAtk > 5) // 밀치기 이후 추가공격 방지
+            {
+                break;
+            }
+
+            yield return new WaitForSeconds(attackCool);
         }
+        
     }
-    
+
     public void Attack()  //애니메이션에 도중 공격을 성공시키기 위한 함수
     {
-        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-        if (rb != null)
+        if (rb != null && randAtk > 5)
         {
             rb.velocity = new Vector2(-2, 0);
         }
-        player.health -= atk; 
+        player.health -= atk;
+
+        if (player.health <= 0)                          // 플레이어가 죽을때까지 반복
+        {
+            player.bFadeCorutine = false;               // 죽었으니 while문 탈출
+        }
     }
 
     public void AnimationOffAtk()       //애니메이션 끝날때 호출
